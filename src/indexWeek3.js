5656fdf3362983db5087fd7597067cc2a55da4c3
36slices <36slices@gmail.com>
2021-03-09 10:53:52 +1300

    indexWeek3 begun

--- src/indexWeek3.js
+++ src/indexWeek3.js
@@ -1,1 +1,38 @@
-console.log("hello from w3")
+console.log("hello from w3 lesgooo")
+//TODO Javascript Variables 
+//--------------------------------------
+// Variables for the code declared here 
+// With strings attached as values.
+//--------------------------------------
+var firstName = 'Matt'
+var lastName = 'Burton'
+
+//--------------------------------------
+//Calling a function to print the value
+// of the string to the terminal.
+//--------------------------------------
+//This can also be on two lines of code,
+// as two console logs.  
+console.log(firstName,lastName) 
+
+//--------------------------------------
+//This is an example of a variable that 
+//has the ability to be updated through
+//the use of the variable "let"
+//--------------------------------------
+//let lastName = 'Burton'
+//console.log(lastName)
+//lastName = 'Smith'
+//console.log(lastName)
+//--------------------------------------
+
+//Assigning a variable to give the first and last name
+//This variable is concatenting the two variables 
+let fullName = firstName + ' ' + lastName
+//-------------------------------------------------------
+//an example of cancatenation through syntax (ES6)
+//fullName = 'My full name is ${firstName} ${lastName}'
+//-------------------------------------------------------
+//Display the concatenated variable.
+console.log(fullName)
+
